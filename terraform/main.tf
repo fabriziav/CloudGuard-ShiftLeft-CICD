a7a0e4b38d65070034ee3ae8f8738d9de5aff382
fabriziav <75419607+fabriziav@users.noreply.github.com>
2022-02-16 14:41:47 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "prov_fw" {
     from_port = 22
     protocol = "tcp"
     to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.168.1.1/32"]
   }
  
   ingress {
